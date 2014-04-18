ce72250602c6c12ba7b4bce51a66ad2e5a4d460a
Jean-Philippe Orsini <jeanfi@gmail.com>
2014-04-18 14:55:57 +0200

    fixed logging

--- src/ui.c
+++ src/ui.c
@@ -151,7 +151,7 @@ const char *ui_get_status_filter()
 	log_fct_enter();
 
 	status = gtk_combo_box_get_active_id(w_status);
-	log_fct("status: %d", status);
+	log_fct("status: %s", status);
 
 	log_fct_exit();
 
