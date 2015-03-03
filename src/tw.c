81823afc60eb19f6cb6411ebde259ad7ec8a3433
Jean-Philippe Orsini <jeanfi@gmail.com>
2015-03-03 07:27:48 +0100

    add taskwarrior 2.4.1 support

--- src/tw.c
+++ src/tw.c
@@ -111,7 +111,8 @@ static int task_check_version()
 	if (!strcmp(ver, "2.2.0")
 	    || !strcmp(ver, "2.0.0")
 	    || !strcmp(ver, "2.3.0")
-	    || !strcmp(ver, "2.4.0"))
+	    || !strcmp(ver, "2.4.0")
+	    || !strcmp(ver, "2.4.1"))
 		return 1;
 	else
 		return 0;
